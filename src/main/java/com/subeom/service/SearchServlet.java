4871e5e58229dd1ad71b925de1f51e8d5769e1b3
Subeom Kwon <subeomkwon@gmail.com>
2024-01-07 01:49:56 +0900

    주석 정리

--- src/main/java/com/subeom/service/SearchServlet.java
+++ src/main/java/com/subeom/service/SearchServlet.java
@@ -12,24 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Initialize your service class
+
         PublicWifiService publicWifiService = new PublicWifiService();
 
         try {
-            // Retrieve user location from the request
             double lat = Double.parseDouble(request.getParameter("lat"));
             double lng = Double.parseDouble(request.getParameter("lng"));
 
-            // Use the service to get nearby wifi spots
+
             List<WifiSpot> wifiSpots = publicWifiService.loadWifiSpots(lat, lng);
 
-            // Set the wifi spots as a request attribute to be accessed by the JSP
             request.setAttribute("wifiSpots", wifiSpots);
 
-            // Forward the request to the JSP page
+
             request.getRequestDispatcher("wifiInfoDisplay.jsp").forward(request, response);
         } catch (Exception e) {
-            // Handle exceptions
+
             e.printStackTrace();
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred while retrieving wifi information.");
         }
