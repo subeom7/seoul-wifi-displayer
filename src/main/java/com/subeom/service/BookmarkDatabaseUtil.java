1d24b98d23c9ea9db9b7f7236ed273a48a6e9bfd
Subeom Kwon <subeomkwon@gmail.com>
2024-01-13 16:47:45 +0900

    북마크 데이터베이스 연동을 위한 유틸리티 클래스 추가

---
+++ src/main/java/com/subeom/service/BookmarkDatabaseUtil.java
@@ -0,0 +1,36 @@
+package com.subeom.service;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookmarkDatabaseUtil {
+
+    public static List<Bookmark> getBookmarks() {
+        List<Bookmark> groups = new ArrayList<>();
+        try {
+            Class.forName("org.sqlite.JDBC");
+            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:database/bookmark.db");
+                 Statement stmt = conn.createStatement()) {
+
+                ResultSet rs = stmt.executeQuery("SELECT * FROM bookmark");
+                while (rs.next()) {
+                    Bookmark bookmark = new Bookmark();
+                    bookmark.setId(rs.getInt("id"));
+                    bookmark.setBookmarkName(rs.getString("bookmarkName"));
+                    bookmark.setWifiName(rs.getString("wifiName"));
+                    bookmark.setAddTimestamp(rs.getString("addTimestamp"));
+                    groups.add(bookmark);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Consider better error handling
+        }
+        return groups;
+    }
+}
+
